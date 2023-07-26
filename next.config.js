852e70493a7e1579a896a206395f92f82001c52a
M1chae1M <michalmaru10@gmail.com>
2023-07-26 14:18:20 +0200

    test bishop 4

--- next.config.js
+++ next.config.js
@@ -1,18 +1,20 @@
 const TerserPlugin = require('terser-webpack-plugin');
-const withTM = require('next-transpile-modules')(['path/to/your_modules']); // Dodaj nazwy modułów, które chcesz traktować jako bezpieczne dla minifikacji
+const withTM = require('next-transpile-modules')([
+  // Dodaj tutaj nazwy modułów, które chcesz traktować jako bezpieczne dla minifikacji
+]);
 
 module.exports = withTM({
   reactStrictMode: true,
 
   webpack: (config, { isServer, dev }) => {
     if (!dev) {
-      config.optimization.minimizer = [
+      config.optimization.minimizer.push(
         new TerserPlugin({
           terserOptions: {
             keep_classnames: true,
           },
-        }),
-      ];
+        })
+      );
     }
 
     return config;

e7b3bc851d12c565c55de3287a99f995c81909bf
M1chae1M <michalmaru10@gmail.com>
2023-07-26 14:16:18 +0200

    new webpack

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
-/** @type {import('next').NextConfig} */
+const TerserPlugin = require('terser-webpack-plugin');
+const withTM = require('next-transpile-modules')(['path/to/your_modules']); // Dodaj nazwy modułów, które chcesz traktować jako bezpieczne dla minifikacji
 
-module.exports = {
+module.exports = withTM({
   reactStrictMode: true,
-  minify: {
-    terserOptions: {
-      keep_classnames: true,
-    },
+
+  webpack: (config, { isServer, dev }) => {
+    if (!dev) {
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            keep_classnames: true,
+          },
+        }),
+      ];
+    }
+
+    return config;
   },
-}
+});

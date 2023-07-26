cb0cb951e7c43ca2fc8bf9df9086d04e5aa84e61
M1chae1M <michalmaru10@gmail.com>
2023-07-26 12:32:37 +0200

    fields no ssr

--- pages/classes/Functions.js
+++ pages/classes/Functions.js
@@ -2,9 +2,13 @@ import React,{Component} from "react";
 import {Xo,Yo} from "../_document";
 
 export function ifBlackFunction(x,y){
-  const isEvenX=x.charCodeAt()%2===0;
-  const isEvenY=y % 2===0;
-  return isEvenX!==isEvenY?'white':'grey'
+  // if (typeof x==='string' && typeof y === 'number') {
+    const isEvenX=x?.charCodeAt?.()%2===0;
+    const isEvenY=y % 2===0;
+    return isEvenX!==isEvenY?'white':'grey'
+  // } else {
+    // return 'white';
+  // }
 }
 
 export default class Functions extends Component{render(){return(<></>)}}

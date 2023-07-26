2c8ec19febfa0dc0008ceff60e46f12e7528acee
M1chae1M <michalmaru10@gmail.com>
2023-07-26 14:28:58 +0200

    new webpack 3

--- next.config.js
+++ next.config.js
@@ -1,19 +1,16 @@
 /** @type {import('next').NextConfig} */
+const TerserPlugin =require('terser-webpack-plugin');
+
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.optimization.minimizer.forEach((minimizer) => {
-        if (minimizer.constructor.name === 'TerserPlugin') {
-          minimizer.options.terserOptions = {
-            ...minimizer.options.terserOptions,
-            keep_classnames: true,
-            keep_fnames: true,
-          };
-        }
-      });
-    }
-    return config;
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_classnames: true,
+        },
+      }),
+    ],
   },
 }
 

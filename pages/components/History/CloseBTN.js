3ebaf78cbff8f88c0ad788ed7252ff0696a018ce
M1chae1M <michalmaru10@gmail.com>
2023-11-17 14:28:59 +0100

    new close button

--- pages/components/History/CloseBTN.js
+++ pages/components/History/CloseBTN.js
@@ -1,30 +1,27 @@
 import {IoClose} from "react-icons/io5";
-
 import styled from "styled-components";
 
-const DV=styled.div`
+const BTN=styled(IoClose)`
   width:fit-content;
   transform-origin: center;
   transition:all 0.5s ease-in-out;
+  color:red;
+  font-size:65px;
   &:hover{
     scale:1.2;
     rotate:90deg;
     transition:all 0.5s ease-in-out;
   }
 `
-const BTN={
-  color:'red',
-  fontSize:'65px',
-}
+
 const style={
   display:'grid',
   justifyContent:'end'
 }
+
 const CloseBTN=(props)=>(
   <div style={style}>
-    <DV {...props}>
-      <IoClose style={BTN}/>
-    </DV>
+    <BTN {...props}/>
   </div>
 )
 

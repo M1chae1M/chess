0836910ccc738ba7f4060630e2395cd2ff76dd0f
M1chae1M <michalmaru10@gmail.com>
2023-11-29 21:27:27 +0100

    secoundClick in functions file

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import History from './components/History/History'
 import {Game} from './classes/Game'
 import AllFields from './components/AllFields'
 import Modal from './components/Modal'
-import {addToHistory,boardModifier,calculateAnimation,checkIsClosed,getBoardFromHistory,getBoardFromLocalStory,resetState,setBoardInLocalStory,touch} from './classes/Functions'
+import {addToHistory,boardModifier,calculateAnimation,checkIsClosed,getBoardFromHistory,getBoardFromLocalStory,resetState,secoundClick,setBoardInLocalStory,touch} from './classes/Functions'
 import CONFIG from '@/config/config.json'
 import GameBoardContainer from './components/GameBoardContainer'
 import AppContainer from './components/AppContainer'
@@ -43,35 +43,7 @@ export default class GameBoard extends Component{
     this.setState(resetState)
     SwitchRef?.current?.changeState?.({start:false});
   }
-  secoundClick=(fromField,clicked)=>{
-    const {whiteTure,firstTouch,isModalOpened,promoteTo,kingAttacked,gameHistory,fiftyMovesRule}=this.state
-    const [destX,destY]=clicked??[]
-    const [acX,acY]=fromField??[]
-    const baseFigure=this.state.boardGameState?.[acX]?.[acY];
-    const isPromotionField=(destY==='8' && this.state.whiteTure)||(destY==='1' && !this.state.whiteTure);
-    const isPawn=baseFigure?.getName?.()==='Pawn';
-    const canMoveThere=baseFigure?.canMove?.(destX,destY,this.state.whiteTure)?.canMove;
-
-    if(isPromotionField && canMoveThere && isPawn){
-      this.setState({isModalOpened:true},()=>new Promise((resolve)=>this.checkIsClosed(resolve,baseFigure,clicked)))
-    }
-    else{
-      if(canMoveThere){
-        Game.getMovesCount();
-        this.setState({canAnimate:true},()=>setTimeout(()=>this.setState({canAnimate:false}),animationTime));
-        this.calculateAnimation(fromField,clicked);
-        this.setState({actualMove:this.state.actualMove+1})
-      }
-      setTimeout(()=>{
-        const {shortMove,newWhiteTure}={...baseFigure?.move?.(destX,destY,this.state.whiteTure)}
-        this.setState({firstTouch:!this.state.firstTouch,boardGameState:shortMove,whiteTure:newWhiteTure},this.setBoardInLocalStory)
-        if(newWhiteTure!==this.state.whiteTure){
-          this.isChequered();
-          this.addToHistory(acX,acY,{from:baseFigure},destX,destY,{whiteTure:newWhiteTure,kingAttacked});
-        }
-      },animationTime)
-    }
-  }
+  secoundClick=secoundClick
   touch=touch.bind(this)
   calculateAnimation=calculateAnimation
   addToHistory=addToHistory

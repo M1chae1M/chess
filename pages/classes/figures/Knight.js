85bab1f2ec738f579023876bbbae9c697ec5f7ef
M1chae1M <michalmaru10@gmail.com>
2023-11-30 12:52:40 +0100

    legalKnightMove

--- pages/classes/figures/Knight.js
+++ pages/classes/figures/Knight.js
@@ -4,13 +4,24 @@ import Yo from '@/config/Yo.json'
 import Xo from '@/config/Xo.json'
 
 export class Knight extends Figure{
+  legalKnightMove(actual,destination){
+    const {acX,acY}=actual
+    const {destX,destY}=destination
+    const horisontalMoveCondition=Math.abs(acX.charCodeAt()-destX.charCodeAt());
+    const condition=(h,w)=>horisontalMoveCondition===h && Math.abs(destY-acY)===w
+    const moreVerticalMove=condition(1,2)
+    const moreHorisontalMove=condition(2,1)
+  
+    return {can:moreVerticalMove||moreHorisontalMove}
+  }
   canMove(destX,destY,whiteTure){
     const moves=[]
     const [acX,acY]=this.actualField;
     const actual={acX,acY}
     const destination={destX,destY}
 
-    if(legalKnightMove(actual,destination).can){
+    // if(legalKnightMove(actual,destination).can){
+    if(this.legalKnightMove(actual,destination).can){
       const tryFields=[[1,2],[1,-2],[-1,2],[-1,-2],[-2,1],[-2,-1],[2,1],[2,-1]];
       tryFields?.map(([x1,x2])=>this.tryField(x1,x2,moves))
 
@@ -59,13 +70,13 @@ export class Knight extends Figure{
 
 export default class T2 extends Component{render(){return(<></>)}}
 
-function legalKnightMove(actual,destination){
-  const {acX,acY}=actual
-  const {destX,destY}=destination
-  const horisontalMoveCondition=Math.abs(acX.charCodeAt()-destX.charCodeAt());
-  const condition=(h,w)=>horisontalMoveCondition===h && Math.abs(destY-acY)===w
-  const moreVerticalMove=condition(1,2)
-  const moreHorisontalMove=condition(2,1)
+// function legalKnightMove(actual,destination){
+//   const {acX,acY}=actual
+//   const {destX,destY}=destination
+//   const horisontalMoveCondition=Math.abs(acX.charCodeAt()-destX.charCodeAt());
+//   const condition=(h,w)=>horisontalMoveCondition===h && Math.abs(destY-acY)===w
+//   const moreVerticalMove=condition(1,2)
+//   const moreHorisontalMove=condition(2,1)
 
-  return {can:moreVerticalMove||moreHorisontalMove}
-}
+//   return {can:moreVerticalMove||moreHorisontalMove}
+// }

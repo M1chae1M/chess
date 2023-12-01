9873920ebb3ea157be8a3d20fab9d18edd0d38de
M1chae1M <michalmaru10@gmail.com>
2023-12-01 13:00:04 +0100

    fixed with 'canStand'

--- pages/components/Field.js
+++ pages/components/Field.js
@@ -15,40 +15,40 @@ export default class Field extends Component{
         const click=()=>touch(`${x}${y}`)
         const base=boardGameState?.[x]?.[y]
         const Figure=()=>base?.getFigure?.()
-        // const isKing=base?.getName?.()==='King'
         const isKing=base?.isKing?.()
         const isColor=base?.goodTure?.(whiteTure)
         const color=ifBlackFunction(x,y)
         const slideInLeft=keyframes`
-        from{
-          transform: translate(0,0);
-        }
-        to{
-          transform:translate(calc(${fieldSize} * ${whiteOnTop?animateX:animateX*(-1)}), calc(${fieldSize} * ${whiteOnTop?animateY*(-1):animateY}));
-        }`;
+          from{
+            transform: translate(0,0);
+          }
+          to{
+            transform:translate(calc(${fieldSize} * ${whiteOnTop?animateX:animateX*(-1)}), calc(${fieldSize} * ${whiteOnTop?animateY*(-1):animateY}));
+          }`;
 
-        const StyledField=styled.div`
-        width:${fieldSize};
-        height:${fieldSize};
-        border:solid black 1px;
-        display:grid;
-        justify-items:center;
-        align-items:center;
-        background:${kingAttacked && isKing && isColor?'red':color};
-        font-size:${fieldSize};
-        transition:all ${animationTime}ms ease-in-out;
+          const StyledField=styled.div`
+          width:${fieldSize};
+          height:${fieldSize};
+          border:solid black 1px;
+          display:grid;
+          justify-items:center;
+          align-items:center;
+          background:${kingAttacked && isKing && isColor?'red':color};
+          font-size:${fieldSize};
+          transition:all ${animationTime}ms ease-in-out;
 
-        & svg{
-          color:${`${x}${y}`===fromField && `red !important`};
-          animation: ${`${x}${y}`===fromField && canAnimate ?css`${slideInLeft} ${animationTime}ms ease-in-out both`:'none'};
-        }
+          & svg{
+            color:${`${x}${y}`===fromField && `red !important`};
+            animation: ${`${x}${y}`===fromField && canAnimate ?css`${slideInLeft} ${animationTime}ms ease-in-out both`:'none'};
+          }
 
-        &:hover{
-          border:solid orangered 5px !important;
-          transform:scale(95%);
-          font-size:max(3vw, 20px) !important;
-          opacity:0.75;
-        }`;
+          &:hover{
+            border:solid orangered 5px !important;
+            transform:scale(95%);
+            font-size:max(3vw, 20px) !important;
+            opacity:0.75;
+          }
+        `;
         return(
           <StyledField onClick={click} className='Field'>
             <Figure style={{

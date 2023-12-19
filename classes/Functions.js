79f19a312c86ee99e65198a810d6396338796eee
M1chae1M <michalmaru10@gmail.com>
2023-12-19 20:23:24 +0100

    secoundClick in other file

--- classes/Functions.js
+++ classes/Functions.js
@@ -9,8 +9,6 @@ import King from "./figures/King";
 import Knight from "./figures/Knight";
 import Rook from "./figures/Rook";
 import _ from "lodash";
-import CONFIG from '@/config/config.json'
-const {animationTime}=CONFIG??''
 
 export function addToHistory(acX,acY,copyOfOldFileds,destX,destY,status){
   this.setState({gameHistory:
@@ -82,34 +80,5 @@ export function touch(clicked){
   else if(!firstTouch){
     this.secoundClick(fromField,clicked);
     Game?.can_NOT_win?.() && this.resetGame();
-  }
-}
-export function secoundClick(fromField,clicked){
-  const {kingAttacked}=this.state
-  const [destX,destY]=clicked??[]
-  const [acX,acY]=fromField??[]
-  const baseFigure=this.state.boardGameState?.[acX]?.[acY];
-  const isPromotionField=(destY==='8' && this.state.whiteTure)||(destY==='1' && !this.state.whiteTure);
-  const isPawn=baseFigure?.getName?.()==='Pawn';
-  const canMoveThere=baseFigure?.canMove?.(destX,destY,this.state.whiteTure)?.canMove;
-
-  if(isPromotionField && canMoveThere && isPawn){
-    this.setState({isModalOpened:true},()=>new Promise((resolve)=>this.checkIsClosed(resolve,baseFigure,clicked)))
-  }
-  else{
-    if(canMoveThere){
-      Game.getMovesCount();
-      this.setState({canAnimate:true},()=>setTimeout(()=>this.setState({canAnimate:false}),animationTime));
-      this.calculateAnimation(fromField,clicked);
-      this.setState({actualMove:this.state.actualMove+1})
-    }
-    setTimeout(()=>{
-      const {shortMove,newWhiteTure}={...baseFigure?.move?.(destX,destY,this.state.whiteTure)}
-      this.setState({firstTouch:!this.state.firstTouch,boardGameState:shortMove,whiteTure:newWhiteTure},this.setBoardInLocalStory)
-      if(newWhiteTure!==this.state.whiteTure){
-        this.isChequered();
-        this.addToHistory(acX,acY,{from:baseFigure},destX,destY,{whiteTure:newWhiteTure,kingAttacked});
-      }
-    },animationTime)
   }
 }
---
+++ functions/secoundClick.js
@@ -0,0 +1,33 @@
+import Game from "@/classes/Game"
+import CONFIG from '@/config/config.json'
+const {animationTime}=CONFIG??''
+
+export function secoundClick(fromField,clicked){
+    const {kingAttacked}=this.state
+    const [destX,destY]=clicked??[]
+    const [acX,acY]=fromField??[]
+    const baseFigure=this.state.boardGameState?.[acX]?.[acY];
+    const isPromotionField=(destY==='8' && this.state.whiteTure)||(destY==='1' && !this.state.whiteTure);
+    const isPawn=baseFigure?.getName?.()==='Pawn';
+    const canMoveThere=baseFigure?.canMove?.(destX,destY,this.state.whiteTure)?.canMove;
+  
+    if(isPromotionField && canMoveThere && isPawn){
+        this.setState({isModalOpened:true},()=>new Promise((resolve)=>this.checkIsClosed(resolve,baseFigure,clicked)))
+    }
+    else{
+        if(canMoveThere){
+            Game.getMovesCount();
+            this.setState({canAnimate:true},()=>setTimeout(()=>this.setState({canAnimate:false}),animationTime));
+            this.calculateAnimation(fromField,clicked);
+            this.setState({actualMove:this.state.actualMove+1})
+        }
+        setTimeout(()=>{
+            const {shortMove,newWhiteTure}={...baseFigure?.move?.(destX,destY,this.state.whiteTure)}
+            this.setState({firstTouch:!this.state.firstTouch,boardGameState:shortMove,whiteTure:newWhiteTure},this.setBoardInLocalStory)
+            if(newWhiteTure!==this.state.whiteTure){
+                this.isChequered();
+                this.addToHistory(acX,acY,{from:baseFigure},destX,destY,{whiteTure:newWhiteTure,kingAttacked});
+            }
+        },animationTime)
+    }
+}

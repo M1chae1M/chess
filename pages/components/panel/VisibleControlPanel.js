12ccb0104923c4a8775e4e3c0443368349fea7c2
M1chae1M <michalmaru10@gmail.com>
2024-01-02 15:50:53 +0100

    responsive panel prototipe

--- pages/components/panel/VisibleControlPanel.js
+++ pages/components/panel/VisibleControlPanel.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import CONFIG from '@/config/config.json'
-const {fieldSize}=CONFIG??{}
+const {fieldSize,size}=CONFIG??{}
 
 const DV=styled.div`
 top:100%;
@@ -11,7 +11,7 @@ grid-auto-flow:row;
 display:grid;
 position:absolute;
 
-@media screen and (min-width: calc(${fieldSize} * 14)){
+@media screen and (min-width: calc(${fieldSize} * 8 + 36px + ${size*4}px)){
     top:50%;
     right:0%;
     transform:translate(calc(100% + 8px),-50%);
